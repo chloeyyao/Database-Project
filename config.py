5608e91a852a53f16d6e311e572d121bf389ed98
Chloe Yao <112351305+chloeyyao@users.noreply.github.com>
2024-12-07 01:31:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-# config.py
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
